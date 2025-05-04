48b8f063b17831d62a5cb71087a1cb44e1119bc6
jRxcky <ricigas@gmail.com>
2025-05-04 12:11:06 +0200

    Create setup_pulp.sh

---
+++ setup_pulp.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e  # Stops the script if any command fails
+
+echo "Installing dependencies..."
+sudo apt-get update
+sudo apt-get install -y build-essential git libftdi-dev libftdi1 doxygen python3-pip libsdl2-dev curl cm
+sudo apt install -y gcc-9 g++-9 cmake python3-pip python3-venv
+
+echo "Setting up Python virtual environment..."
+python3 -m venv myenv
+source myenv/bin/activate
+pip install argcomplete pyelftools six prettytable
+deactivate
+
+echo "Cloning PULP SDK..."
+git clone https://github.com/pulp-platform/pulp-sdk.git
+
+echo "Downloading and extracting PULP RISC-V GNU toolchain..."
+cd $HOME/Downloads
+wget https://github.com/pulp-platform/pulp-riscv-gnu-toolchain/releases/download/v1.0.16/v1.0.16-pulp-riscv-gcc-ubuntu-18.tar.bz2
+mv v1.0.16-pulp-riscv-gcc-ubuntu-18.tar.bz2 $HOME
+cd $HOME
+tar -xvjf v1.0.16-pulp-riscv-gcc-ubuntu-18.tar.bz2
+rm v1.0.16-pulp-riscv-gcc-ubuntu-18.tar.bz2
+
+echo "Configuring environment..."
+source myenv/bin/activate
+export PULP_RISCV_GCC_TOOLCHAIN=$HOME/v1.0.16-pulp-riscv-gcc-ubuntu-18
+cd pulp-sdk
+source configs/pulp-open.sh
+
+echo "Building PULP SDK..."
+CC=gcc-9 CXX=g++-9 make all
+
+echo "Testing setup..."
+cd tests/cluster/fork
+make clean all run runner_args="--trace=insn"
+
+echo "Setup complete!"
