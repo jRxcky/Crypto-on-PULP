e4a633ea66c618b1622b958851771653eb28b6f2
jRxcky <ricigas@gmail.com>
2025-05-08 11:08:27 +0200

    Update setup_pulp.sh

--- setup_pulp.sh
+++ setup_pulp.sh
@@ -7,13 +7,14 @@ sudo apt-get install -y build-essential git libftdi-dev libftdi1 doxygen python3
 sudo apt install -y gcc-9 g++-9 cmake python3-pip python3-venv
 
 echo "Setting up Python virtual environment..."
+cd
 python3 -m venv myenv
 source myenv/bin/activate
-pip install argcomplete pyelftools six prettytable
+pip install six
+sudo apt install python3-prettytable python3-argcomplete python3-pyelftools
 deactivate
 
 echo "Cloning PULP SDK..."
-cd
 git clone https://github.com/pulp-platform/pulp-sdk.git
 
 echo "Downloading and extracting PULP RISC-V GNU toolchain..."
